751131262f4362250c3375b074bfe353c6e2e7a9
darellanodev <darellano.aliaga@gmail.com>
2024-09-25 21:31:51 +0200

    closes #1 refactor

--- js/01-passengers/src/PassengersManager.js
+++ js/01-passengers/src/PassengersManager.js
@@ -1,10 +1,13 @@
 class PassengersManager {
+  static UP = 0
+  static DOWN = 1
+
   countPassengersAtLastStation(data) {
     let upPassengers = 0
     let downPassengers = 0
     for (let station of data) {
-      upPassengers += station[0]
-      downPassengers += station[1]
+      upPassengers += station[PassengersManager.UP]
+      downPassengers += station[PassengersManager.DOWN]
     }
     return upPassengers - downPassengers
   }
