46aacbf15a765ec6aeeba02c1ebb557378fde32b
darellanodev <darellano.aliaga@gmail.com>
2024-12-22 19:18:40 +0100

    Improve the test description (#11)

--- katas/02-robot-translator/js/tests/Translator.test.js
+++ katas/02-robot-translator/js/tests/Translator.test.js
@@ -1,7 +1,7 @@
 const Translator = require('../src/Translator');
 
 describe("translator", () => {
-  it("translate the robot string to human readable format", () => {
+  it("translate the robot string to human readable format replacing the characters 3, 0 and %", () => {
     const translator = new Translator();
     expect(translator.translate("H3ll0%w0rld")).toBe("Hello world");
   });
